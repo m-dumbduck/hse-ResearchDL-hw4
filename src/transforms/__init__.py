9db4c5f1bdde64afe40c4eeb95ee4e9b428a8502
Maksim Lomakin <malomakin@edu.hse.ru>
2026-05-15 23:23:15 +0300

    added inference crop

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,4 +1,5 @@
 from src.transforms.compose import Compose
+from src.transforms.crop_to_length import CropToLength
 from src.transforms.pad_with_replicate import PadWithReplicate
 from src.transforms.permute import Permute
 from src.transforms.random_crop import RandomCrop1D
